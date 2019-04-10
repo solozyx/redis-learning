392485b6437552657705289dfc2712072607f370
solozyx <zhaoyingxin@aliyun.com>
2019-04-10 11:13:55 +0800

    redis常用命令

---
+++ cmd/setup.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+mkdir -p /opt/soft
+wget http://download.redis.io/releases/redis-3.0.7.tar.gz
+tar -zxf redis-3.0.7.tar.gz
+# 创建软连接
+ln -s redis-3.0.7 redis
+cd redis
+make
+make install
+redis-server
+redis-cli -h 127.0.0.1 -p 6379
+
+redis-server --port 6380
+
+ps -ef | grep redis-server
+ps -ef | grep redis-server | grep -v "grep"
+
+mkdir config
+cp redis.conf config/
+cat redis.conf | grep -v "#" | grep -v "^$" > redis-6382.conf
+
+
